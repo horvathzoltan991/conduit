05af9264a68d0b6573740a9ec55c26baf202cd3f
Zoltán Horváth <horvathzoltan991@gmail.com>
2022-05-25 16:37:14 +0200

    added data.py

---
+++ Test_Conduit/data.py
@@ -0,0 +1,12 @@
+test_data = {
+    'valid_email': 'hzoltan@gmail.com',
+    'valid_password': 'Jelszo123',
+    'invalid_username': '',
+    'invalid_email': 'proba@gmail.com',
+    'invalid_password': 'probajelszo',
+    'article_title': 'Test Title',
+    'article_summary': 'Test Summary',
+    'article_text': 'Test Text',
+    'article_tag': 'Test Tag',
+    'test_comment': 'This is a comment!'
+}
