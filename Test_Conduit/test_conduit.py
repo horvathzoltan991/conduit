243ee56831f2710c387802a195a541ae1be6751e
Zoltán Horváth <horvathzoltan991@gmail.com>
2022-06-02 13:29:33 +0200

    headless option

--- Test_Conduit/test_conduit.py
+++ Test_Conduit/test_conduit.py
@@ -37,8 +37,7 @@ class TestConduit(object):
     # TC03 - Bejelentkezés (helyes adatokkal)
     def test_login(self):
         login(self.browser, test_data['valid_email'], test_data['valid_password'])
-        profile_btn = WebDriverWait(self.browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//a[@href="#/@hzoltan/" and @class="nav-link"]')))
+        profile_btn = self.browser.find_element_by_xpath('//a[@href="#/@hzoltan/" and @class="nav-link"]')
         assert profile_btn.text == 'hzoltan'
 
     # TC04 - Adatok listázása (tagek listázása)
